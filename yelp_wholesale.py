5d31a8f2fdd804cc8616cd0eca996ea0a5f2c6a9
anku1903 <ankurzalavadiya11@gmail.com>
2024-04-10 13:24:56 +0530

    25

--- yelp_wholesale.py
+++ yelp_wholesale.py
@@ -19,34 +19,14 @@ TABLE_NAME = "restaurant"
 GET_QUERY = '''SELECT name, min(url) as url from wholesale WHERE rating = '' GROUP BY name;'''
 
 def scrape_yelp_profile(url, proxy=None):
-    ua_list = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.59 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0.2",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0.2",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1",
-    "Mozilla/5.0 (Linux; Android 11; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 YaBrowser/21.6.4.787 Yowser/2.5 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 BIDUBrowser/2.x Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Iron Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Comodo_Dragon/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Amigo/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Vivaldi/4.1 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Maxthon/6.1.3.2000 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Qutebrowser/2.1.1 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Coc_Coc_browser/100.2.175 Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Whale/2.10.115.42 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Sleipnir/6.5.16 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 YandexBrowser/21.6.4.146 Yowser/2.5 Safari/537.36"
-]
-
+    ua_path = os.path.abspath('ua.txt')
+    ua_list = open(ua_path,'r').readlines()
 
 
     driver_linuxpath = "/usr/bin/chromedriver/chromedriver"
+    driver_path = "G:/aws/aws-scraper/chromedriver.exe"
     options = Options()
-    options.add_argument('--headless=new')
+    # options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
@@ -54,7 +34,7 @@ def scrape_yelp_profile(url, proxy=None):
     options.add_argument('--start-maximized')
     options.add_argument(f'user-agent={random.choice(ua_list)}')
 
-    driver_service = Service(executable_path=driver_linuxpath)
+    driver_service = Service(executable_path=driver_path)
 
 
     if proxy:
@@ -66,7 +46,7 @@ def scrape_yelp_profile(url, proxy=None):
 
     try:
         driver.get(url)
-        time.sleep(7)  # Adjust per your need
+        time.sleep(10)  # Adjust per your need
 
         content = driver.page_source
         soup = BeautifulSoup(content, 'html.parser')
@@ -86,7 +66,13 @@ def scrape_yelp_profile(url, proxy=None):
             if rating_element:
                 rating = rating_element.get_text()
         
-        website_element = soup.select_one('body > yelp-react-root > div:nth-child(1) > div.biz-details-page-container-outer__09f24__pZBzx.css-1qn0b6x > div > div.css-s97lou > div.css-u9z0t a[href*="/biz_redir"]')
+        website_element = soup.find_all('a')
+
+        for element in website_element:
+            # Check if the href attribute starts with "/biz_redir"
+            if element.get('href') and element.get('href').startswith("/biz_redir"):
+                website = element.text
+    
 
         category_element = soup.select_one('body > yelp-react-root > div:nth-child(1) > div.photoHeader__09f24__nPvHp.css-1qn0b6x > div.photo-header-content-container__09f24__jDLBB.css-1qn0b6x > div.photo-header-content__09f24__q7rNO.css-2wl9y > div > div > span.css-1xfc281 > span:nth-child(1) > a')
         if website_element:
@@ -134,7 +120,7 @@ def scrape_multiple_urls(urls):
 if __name__ == "__main__":
     start_time = time.perf_counter()
 
-    url = "https://www.yelp.com/biz/cosmoprof-glendale?osq=Wholesalers"
+    url = "https://www.yelp.com/biz/san-antonio-flowers-san-antonio?osq=Wholesalers"
 
     scrape_yelp_profile(url)
 
